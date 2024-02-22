df3097963321b307ba2002ed90ac38c0c7997cc3
michele-pupo <mikypupo@hotmail.it>
2024-02-22 16:53:54 +0100

    milestone 1 complete

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ createApp({
           contacts: [
             {
                 name: 'Michele',
-                avatar: './img/avatar_1.png',
+                avatar: 'img/avatar_1.jpg',
                 visible: true,
                 messages: [
                     {
@@ -42,7 +42,7 @@ createApp({
             },
             {
                 name: 'Fabio',
-                avatar: './img/avatar_2.png',
+                avatar: 'img/avatar_2.jpg',
                 visible: true,
                 messages: [
                     {
@@ -64,7 +64,7 @@ createApp({
             },
             {
                 name: 'Samuele',
-                avatar: './img/avatar_3.png',
+                avatar: 'img/avatar_3.jpg',
                 visible: true,
                 messages: [
                     {
@@ -86,7 +86,7 @@ createApp({
             },
             {
                 name: 'Alessandro B.',
-                avatar: './img/avatar_4.png',
+                avatar: 'img/avatar_4.jpg',
                 visible: true,
                 messages: [
                     {
@@ -103,7 +103,7 @@ createApp({
             },
             {
                 name: 'Alessandro L.',
-                avatar: './img/avatar_5.png',
+                avatar: 'img/avatar_5.jpg',
                 visible: true,
                 messages: [
                     {
@@ -120,7 +120,7 @@ createApp({
             },
             {
                 name: 'Claudia',
-                avatar: './img/avatar_6.png',
+                avatar: 'img/avatar_6.jpg',
                 visible: true,
                 messages: [
                     {
@@ -142,7 +142,7 @@ createApp({
             },
             {
                 name: 'Federico',
-                avatar: './img/avatar_7.png',
+                avatar: 'img/avatar_7.jpg',
                 visible: true,
                 messages: [
                     {
@@ -159,7 +159,7 @@ createApp({
             },
             {
                 name: 'Davide',
-                avatar: './img/avatar_8.png',
+                avatar: 'img/avatar_8.jpg',
                 visible: true,
                 messages: [
                     {
