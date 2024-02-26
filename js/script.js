079a15a22ee321d1675a9595ebf146cc6d1e26d8
michele-pupo <mikypupo@hotmail.it>
2024-02-26 16:04:23 +0100

    added ability to mark important messages

--- js/script.js
+++ js/script.js
@@ -60,19 +60,22 @@ createApp({
                         date: '10/01/2020 15:30:55',
                         message: 'Hai portato a spasso il cane?',
                         status: 'sent',
-                        hasResponse: false
+                        hasResponse: false,
+                        isImportant: false
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Ricordati di stendere i panni',
                         status: 'sent',
-                        hasResponse: false
+                        hasResponse: false,
+                        isImportant: false
                     },
                     {
                         date: '10/01/2020 16:15:22',
                         message: 'Tutto fatto!',
                         status: 'received',
-                        hasResponse: false
+                        hasResponse: false,
+                        isImportant: false
                     }
                 ],
             },
@@ -85,19 +88,22 @@ createApp({
                         date: '20/03/2020 16:30:00',
                         message: 'Ciao come stai?',
                         status: 'sent',
-                        hasResponse: false
+                        hasResponse: false,
+                        isImportant: false
                     },
                     {
                         date: '20/03/2020 16:30:55',
                         message: 'Bene grazie! Stasera ci vediamo?',
                         status: 'received',
-                        hasResponse: false
+                        hasResponse: false,
+                        isImportant: false
                     },
                     {
                         date: '20/03/2020 16:35:00',
                         message: 'Mi piacerebbe ma devo andare a fare la spesa.',
                         status: 'sent',
-                        hasResponse: false
+                        hasResponse: false,
+                        isImportant: false
                     }
                 ],
             },
@@ -110,18 +116,21 @@ createApp({
                         date: '28/03/2020 10:10:40',
                         message: 'La Marianna va in campagna',
                         status: 'received',
-                        hasResponse: false
+                        hasResponse: false,
+                        isImportant: false
                     },
                     {
                         date: '28/03/2020 10:20:10',
                         message: 'Sicuro di non aver sbagliato chat?',
                         status: 'sent',
-                        hasResponse: false
+                        hasResponse: false,
+                        isImportant: false
                     },
                     {
                         date: '28/03/2020 16:15:22',
                         message: 'Ah scusa!',
                         status: 'received',
+                        hasResponse: false,
                         hasResponse: false
                     }
                 ],
@@ -135,12 +144,14 @@ createApp({
                         date: '10/01/2020 15:30:55',
                         message: 'Lo sai che ha aperto una nuova pizzeria?',
                         status: 'sent',
+                        hasResponse: false,
                         hasResponse: false
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Si, ma preferirei andare al cinema',
                         status: 'received',
+                        hasResponse: false,
                         hasResponse: false
                     }
                 ],
@@ -154,12 +165,14 @@ createApp({
                         date: '10/01/2020 15:30:55',
                         message: 'Ricordati di chiamare la nonna',
                         status: 'sent',
+                        hasResponse: false,
                         hasResponse: false
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Va bene, stasera la sento',
                         status: 'received',
+                        hasResponse: false,
                         hasResponse: false
                     }
                 ],
@@ -173,18 +186,21 @@ createApp({
                         date: '10/01/2020 15:30:55',
                         message: 'Ciao Claudia, hai novità?',
                         status: 'sent',
+                        hasResponse: false,
                         hasResponse: false
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Non ancora',
                         status: 'received',
+                        hasResponse: false,
                         hasResponse: false
                     },
                     {
                         date: '10/01/2020 15:51:00',
                         message: 'Nessuna nuova, buona nuova',
                         status: 'sent',
+                        hasResponse: false,
                         hasResponse: false
                     }
                 ],
@@ -198,12 +214,14 @@ createApp({
                         date: '10/01/2020 15:30:55',
                         message: 'Fai gli auguri a Martina che è il suo compleanno!',
                         status: 'sent',
+                        hasResponse: false,
                         hasResponse: false
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Grazie per avermelo ricordato, le scrivo subito!',
                         status: 'received',
+                        hasResponse: false,
                         hasResponse: false
                     }
                 ],
@@ -217,18 +235,21 @@ createApp({
                         date: '10/01/2020 15:30:55',
                         message: 'Ciao, andiamo a mangiare la pizza stasera?',
                         status: 'received',
+                        hasResponse: false,
                         hasResponse: false
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                         status: 'sent',
+                        hasResponse: false,
                         hasResponse: false
                     },
                     {
                         date: '10/01/2020 15:51:00',
                         message: 'OK!!',
                         status: 'received',
+                        hasResponse: false,
                         hasResponse: false
                     }
                 ],
@@ -390,6 +411,11 @@ createApp({
         lastMessageDate(contact) {
             const lastMessage = contact.messages[contact.messages.length - 1];
             return DateTime.fromFormat(lastMessage.date, 'dd/MM/yyyy HH:mm:ss');
+        },
+
+        // aggiunta della stellina per il messaggio importante
+        toggleMessageImportance(message) {
+            message.isImportant = !message.isImportant;
         },
 
         // funzione che richiama le varie funzioni di cambio thema
